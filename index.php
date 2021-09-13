f0ccb38f518be16c4ec32e763f395163380e77d4
alexfiles <82926068+alexfiles@users.noreply.github.com>
2021-09-13 05:47:19 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<lottie-player id="eightthLottie" src="https://assets8.lottiefiles.com/packages/lf20_tfqocvhu.json" style="width:400px; height: 400px;">"></lottie-player>
+
+<script>
+LottieInteractivity.create({
+    mode:"cursor",
+    player: "#eightthLottie",
+    actions: [
+        {
+            position: { x: [0, 1], y: [0, 1] },
+            type: "seek",
+            frames: [0, 600]
+        }
+    ]
+});
+</script>
